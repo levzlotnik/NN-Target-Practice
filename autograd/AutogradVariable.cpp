547716a80035957c684ae3107189809f17bc73f1
levzlotnik <w.lev.zlotnik@gmail.com>
2020-06-20 09:02:54 +0300

    Removed TODO, because it turned out to be fine (the jacobian is constant throughout the flow, so accumulating gradients is fine at any point).

--- autograd/AutogradVariable.cpp
+++ autograd/AutogradVariable.cpp
@@ -4,11 +4,11 @@
 
 #include "AutogradVariable.h"
 
-void AutogradVariable::accumulate_grad(const Vector &jac) {
-    if (requires_jac_init)
-        this->jac = jac;
+void AutogradVariable::accumulate_grad(const Vector &grad) {
+    if (ptr_grad_data == nullptr)
+        ptr_grad_data = shared_ptr<Vector>(grad.clone());
     else
-        this->jac += jac;
+        this->grad() += grad;
 }
 
 void AutogradVariable::forward() {
@@ -23,24 +23,13 @@ void AutogradVariable::backward(const Vector& current_grad, bool recursive) {
         auto jac = source_functor_ptr->jac(i, args, this->data);
         auto dep_grad = matmul(current_grad, *jac);
         dependencies[i]->accumulate_grad(dep_grad);
-         // TODO - this is not good. For instance - if we have a skip-connection:
-         //  h_{n+2} = h_{n} + h_{n+1}(h_{n})
-         //  we have 2 gradient paths: (1) h_{n+2}->h_{n}; (2) h_{n+2}->h_{n+1}->h_{n}.
-         //  After (1) finishes - this recursive call makes him call `.backward()` instantly, without waiting
-         //  for (2) to finish. We need a graph traversal that allows to wait until all gradients got to the
-         //  variable and only then continue the recursive call. For instance - we can have a constant
-         //  unordered_set<Variable*> dependees, and each call to backward that gets to some variable it removes a
-         //  dependee (resets the set after calling `.zero_grad()`). This allows us to accumulate the correct gradient
-         //  before proceeding to go to next nodes.
         if (recursive)
             dependencies[i]->backward(dep_grad, true);
     }
 }
 
 void AutogradVariable::zero_grad(bool recursive) {
-    if (requires_jac_init)
-        warning::warn("Warning: zeroing jacobian without initializing it is will have no effect.");
-    this->jac.apply_([](float& x) {return 0;});
+    this->grad().apply_([](float& x) {return 0;});
     if(recursive)
         for(auto dep: dependencies)
             dep->zero_grad(true);
