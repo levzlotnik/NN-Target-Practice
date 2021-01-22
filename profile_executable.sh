f663095ba7d5fff93a5d7dfa4934d426cf7bc0c2
Lev Zlotnik <46742999+levzlotnik@users.noreply.github.com>
2021-01-21 16:25:30 -0800

    Make script more verbose

--- profile_executable.sh
+++ profile_executable.sh
@@ -1,10 +1,15 @@
-EXEC_CMD=$1
-EXEC_FILENAME=$(basename -- "$EXEC_CMD")
+#!/bin/bash
+
+EXEC_CMD="$@"
+echo "$EXEC_CMD"
+FIRST_ARG=$1
+EXEC_FILENAME=$(basename -- "$FIRST_ARG")
 CALLGRIND_OUT_FILENAME="$EXEC_FILENAME.out"
 DOT_FILENAME="$EXEC_FILENAME.dot"
+STACK_SIZE=10000000
 
-valgrind --tool=callgrind --callgrind-out-file="$CALLGRIND_OUT_FILENAME" $EXEC_CMD
-gprof2dot --format=callgrind --output="$DOT_FILENAME" "$CALLGRIND_OUT_FILENAME"
-dot -Tsvg "$DOT_FILENAME" -o "$EXEC_FILENAME.profile.svg"
-rm "$CALLGRIND_OUT_FILENAME"
+valgrind --tool=callgrind --main-stacksize=$STACK_SIZE --callgrind-out-file="$CALLGRIND_OUT_FILENAME" $EXEC_CMD
+gprof2dot --format=callgrind --output="$DOT_FILENAME" "$CALLGRIND_OUT_FILENAME" && \
+dot -Tsvg "$DOT_FILENAME" -o "$EXEC_FILENAME.profile.svg" && \
+rm "$CALLGRIND_OUT_FILENAME" && \
 rm "$DOT_FILENAME"
